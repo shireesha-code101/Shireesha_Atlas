032f9c8bb4b89f6b68d746fd6c063f64a2cd9ba5
shireesha-code101 <shireeshadcgowda@gmail.com>
2025-06-06 13:15:44 +0530

    files

---
+++ src/task009.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class task009 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("enter the first number: ");
+            int num1 = scanner.nextInt();
+            System.out.print("enter the second number: ");
+            int num2 = scanner.nextInt();
+            if(num1 > num2)
+
+            {
+                System.out.println("num1 is greater :" + num1);
+            }
+            else
+
+            {
+                System.out.println("num2 is greater :" + num2);
+            }
+        }
+    }
+
+

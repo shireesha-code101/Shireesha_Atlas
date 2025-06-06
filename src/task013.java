4139c3d11d48c3b69a802b67d252ad205896bde0
shireesha-code101 <shireeshadcgowda@gmail.com>
2025-06-06 16:05:31 +0530

    files

---
+++ src/task013.java
@@ -0,0 +1,10 @@
+public class task013 {
+    public static void main(String[] args){
+        for(int i=10;i>0;i--){
+            if(i==7||i==5){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}

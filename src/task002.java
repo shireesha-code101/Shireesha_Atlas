1f005e9b2254b0c7dbd4ce6663a41178747871d6
shireesha-code101 <shireeshadcgowda@gmail.com>
2025-06-14 08:04:49 +0530

    Update task002.java

--- src/task002.java
+++ src/task002.java
@@ -1,2 +1,11 @@
-public class task002 {
+public class task002{
+    public static void add(int a,int b){
+        int sum = a + b;
+        System.out.println("sum is :" + sum);
+    }
+    public static void main(String[] args){
+        add(2,4);
+        add(3,4);
+        add(10,20);
+    }
 }
